c21948a2e06c22106878cc1f6aa385f36d7253f1
Mike Mclaren <mike.mclaren@sq1.com>
2016-04-28 14:03:07 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Slack Jotch Says
+This is a way to get the messages from one slack channel / slack team and post it to another.
+
+No specific reason this exists.
---
+++ server.js
@@ -0,0 +1,38 @@
+require('babel-polyfill');
+import {
+  RtmClient,
+  MemoryDataStore,
+  CLIENT_EVENTS,
+  RTM_EVENTS,
+  WebClient
+} from '@slack/client';
+
+const sourceToken = process.env.SOURCE_SLACK_API_TOKEN;
+const destinationToken = process.env.DESTINATION_SLACK_API_TOKEN;
+
+const rtm = new RtmClient(sourceToken, {
+  logLevel: 'debug',
+  dataStore: new MemoryDataStore()
+});
+const web = new WebClient(destinationToken);
+
+rtm.start();
+
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (startData) => {
+  console.log(`Logged in as ${startData.self.name} of team ${startData.team.name}, but not connected to a channel yet`);
+});
+
+rtm.on(RTM_EVENTS.MESSAGE, (message) => {
+  if(message.channel === process.env.SOURCE_SLACK_CHANNEL) {
+    const user = rtm.dataStore.getUserById(message.user);
+
+    let text = message.text;
+    const attachments = message.attachments;
+
+    text = user.name + '\n' + text;
+    web.chat.postMessage(process.env.DESTINATION_SLACK_CHANNEL, text, {
+      username: 'Jotch Bot',
+      attachments
+    }, () => {});
+  }
+});
